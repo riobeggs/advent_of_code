83f3befda4544beea42125bcb54cdcbba5b80531
Rio <riobeggs1@gmail.com>
2022-11-01 00:31:49 +1300

    updated doc strings and finished day 2 part 2

--- advent_of_code_2021/day2.py
+++ advent_of_code_2021/day2.py
@@ -1,7 +1,7 @@
 from util.input import get_input
 
 
-class Dive:
+class Dive1:
     def __init__(self):
         self._input = get_input("day2")
         self._horizontal_position = 0
@@ -10,7 +10,7 @@ class Dive:
 
     def find_positions(self):
         """
-        Finds the horizonal position after all forward moves in self._input.
+        Finds positions after all moves in self._input.
         """
         for move in self._input:
 
@@ -35,14 +35,56 @@ class Dive:
     def run(self):
         self.find_positions()
         self.find_coverage()
-        print(self._coverage)
+        print(f"Coverage: {self._coverage}")
+
+
+class Dive2:
+    def __init__(self):
+        self._input = get_input("day2")
+        self._horizontal_position = 0
+        self._vertical_position = 0
+        self._aim = 0
+        self._coverage = None
+
+    def find_positions(self):
+        """
+        Finds positions after all moves in self._input.
+        """
+        for move in self._input:
+
+            if "forward" in move:
+                amount = move.split(" ")
+                self._horizontal_position += int(amount[1])
+                self._vertical_position += self._aim * int(amount[1])
+
+            elif "up" in move:
+                amount = move.split(" ")
+                self._aim -= int(amount[1])
+
+            elif "down" in move:
+                amount = move.split(" ")
+                self._aim += int(amount[1])
+
+    def find_coverage(self):
+        """
+        Total coverage by multiplying the final vertical and horizontal positions.
+        """
+        self._coverage = self._horizontal_position * self._vertical_position
+
+    def run(self):
+        self.find_positions()
+        self.find_coverage()
+        print(f"Coverage: {self._coverage}")
 
 
 def main():
     """Part 1"""
-    dive = Dive()
-    dive.run()
+    # dive1 = Dive1()
+    # dive1.run()
 
+    """Part 2"""
+    dive2 = Dive2()
+    dive2.run()
 
 if __name__ == "__main__":
     main()
